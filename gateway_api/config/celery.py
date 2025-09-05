7131d8a3cbc7b86de2eebcb001d589659fe0b529
light-hat <pirogov30002@gmail.com>
2025-09-05 17:11:07 +0300

    update celery

--- gateway_api/config/celery.py
+++ gateway_api/config/celery.py
@@ -7,6 +7,6 @@ import os
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings.prod")
-celery_app = Celery("conf")
+celery_app = Celery("config")
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
 celery_app.autodiscover_tasks()
