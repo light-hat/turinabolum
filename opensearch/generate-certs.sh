d38a4e2e8a12efa2ec5d70f6261bf587df728697
light-hat <pirogov30002@gmail.com>
2025-09-05 17:17:10 +0300

    update celery

--- opensearch/generate-certs.sh
+++ opensearch/generate-certs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Generate certificates for your OpenSearch cluster
 
-OPENDISTRO_DN="/C=FR/ST=IDF/L=PARIS/O=EXAMPLE"   # Edit here and in opensearch.yml
+OPENDISTRO_DN="/C=FR/ST=IDF/L=PARIS/O=EXAMPLE"
 
 mkdir -p certs/{ca,os-dashboards}
 
