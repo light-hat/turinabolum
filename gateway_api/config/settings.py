f55831f70b3d5af7e567a7284dfca8e1c64a8207
light-hat <pirogov30002@gmail.com>
2025-09-09 13:55:50 +0300

    update entrypoint.sh

--- gateway_api/config/settings.py
+++ gateway_api/config/settings.py
@@ -263,8 +263,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = STATIC_DIR
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
